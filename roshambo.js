96129e82fb9a764dbfd19b02d80f38583ff42f4c
d-u-b-s <119791407+d-u-b-s@users.noreply.github.com>
2023-02-10 12:29:04 +0100

    added playRound function

--- roshambo.js
+++ roshambo.js
@@ -10,13 +10,63 @@ function getComputerChoice() {
     } else {
         computerChoice = "gun";
     }
+    console.log(computerChoice);
     return computerChoice;
 };
 
 function getPlayerChoice() {
     let playerChoice = prompt("rock, paper, or scissors? ");
     playerChoice = playerChoice.toLowerCase();
-    console.log = playerChoice;
+        // if (playerChoice !== "rock" || playerChoice !== "paper" || playerChoice !== "scissors" || playerChoice !== "gun") {
+        //     playerChoice = "Invalid";
+        //     return playerChoice;
+        // }
+    console.log(playerChoice);
     return playerChoice;
 }
 
+function playRound() {
+    let playerChoice = getPlayerChoice();    
+    let computerChoice = getComputerChoice();
+    
+    
+    let result = "null";
+    if (computerChoice == "gun" && playerChoice != 'gun') {
+        result = "lose";
+        console.log(result);
+        return result;
+    } else if (computerChoice == "rock" && playerChoice == 'scissors') {
+        result = "lose";
+        console.log(result);
+        return result;
+    } else if (computerChoice == "scissors" && playerChoice == 'paper') {
+            result = "lose";
+            console.log(result);
+            return result;
+    } else if (computerChoice == "paper" && playerChoice == 'rock') {
+        result = "lose";
+        console.log(result);
+        return result;
+    } else if (computerChoice != "gun" && playerChoice == 'gun') {
+            result = "win";
+            console.log(result);
+            return result;
+    } else if (computerChoice == "rock" && playerChoice == 'paper') {
+        result = "win";
+        console.log(result);
+        return result;
+    } else if (computerChoice == "paper" && playerChoice == 'scissors') {
+        result = "win";
+        console.log(result);
+        return result;
+    } else if (computerChoice == "scissors" && playerChoice == 'rock') {
+        result = "win";
+        console.log(result);
+        return result;
+    } else if (computerChoice === playerChoice) {
+        result = 'tie';
+        console.log(result);
+        return result;    
+    }
+    
+};
